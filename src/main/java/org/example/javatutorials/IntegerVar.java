83bd6f064574c775d266b1cc75665872ca5dd89a
cocoiscat <demy176@naver.com>
2023-06-30 16:06:17 +0900

    feat: "변수" 예제 코드

---
+++ src/main/java/org/example/javatutorials/IntegerVar.java
@@ -0,0 +1,12 @@
+package org.example.javatutorials;
+
+public class Variable {
+    public static void main(String[] args) {
+        int a;
+        a = 1;
+        System.out.println(a + 1);
+
+        a = 2;
+        System.out.println(a + 1);
+    }
+}
