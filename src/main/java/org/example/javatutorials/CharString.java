f567eede01cf02370eb8bee4ab9caf4beed9ff8a
cocoiscat <demy176@naver.com>
2023-06-30 15:11:57 +0900

    feat: "문자" 예제 코드

---
+++ src/main/java/org/example/javatutorials/CharString.java
@@ -0,0 +1,16 @@
+package org.example.javatutorials;
+
+public class CharString {
+    public static void main(String[] args) {
+        /**
+         * 자바는 문자(Character)와 문자열(String)은 다르다
+         */
+
+        System.out.println('생');
+        System.out.println("생활코딩");
+        System.out.println("생");
+        System.out.println("생활코딩" + "입니다");
+        System.out.println("1" + "1");
+        System.out.println("egonig said \"Welcome programming world\"");
+    }
+}
