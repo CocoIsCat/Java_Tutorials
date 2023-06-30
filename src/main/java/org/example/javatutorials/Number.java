18177bd9166e605fa22ca4df21aa094d7d8568c8
cocoiscat <demy176@naver.com>
2023-06-30 13:24:30 +0900

    feat: "숫자" 예제 코드

---
+++ src/main/java/org/example/javatutorials/Number.java
@@ -0,0 +1,19 @@
+package org.example.javatutorials;
+
+public class Number {
+    public static void main(String[] args) {
+
+        // 자료형 - 정수
+        System.out.println(1+2);
+
+        //자료형 - 실수
+        System.out.println(1.2 + 2.3);
+
+        //곱하기
+        System.out.println(2 * 5);
+
+        //나누기
+        System.out.println(6 / 2);
+
+    }
+}
