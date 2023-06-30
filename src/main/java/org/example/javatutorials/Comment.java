4272cf42e641615e4704c7bfab46dc7346cbd486
cocoiscat <demy176@naver.com>
2023-06-30 16:22:01 +0900

    feat: "주석과 세미콜론" 예제 코드

---
+++ src/main/java/org/example/javatutorials/Comment.java
@@ -0,0 +1,20 @@
+package org.example.javatutorials;
+
+public class Comment {
+    public static void main(String[] args) {
+        //한 줄 주석
+        String a, b;
+
+        /* 여러 줄 주석
+        * a = "coding";
+        * b = " everybody"
+        * */
+
+        /** JavaDoc 주석
+         * 에베베벱베
+         */
+
+        //세미콜론
+        //자바에서는 문장의 끝에 세미콜론을 사용해야 한다
+    }
+}
